e6df71c12a48226dc80518f448ee55695140aff8
Gustav Solis <guso9085@colorado.edu>
2020-02-29 16:46:36 -0700

    Initial Commit

---
+++ ArtTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ArtTracker
+//
+//  Created by Gustav Solis on 2/29/20.
+//  Copyright © 2020 Gustav Solis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
